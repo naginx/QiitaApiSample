72a2aba62604d1798a26a6d337023b46cd81082d
ハナウタ三浦 <miura.hnut@gmail.com>
2020-05-03 11:47:56 +0900

    選択した記事をブラウザで開ける機能を追加

--- QiitaApiSample/ViewController.swift
+++ QiitaApiSample/ViewController.swift
@@ -8,26 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
-    
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     var titleArray: [String] = []
+    var urlArray: [String] = []
+    var alertController: UIAlertController!
+    
+    // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func viewWillAppear(_ animated: Bool) {
         getQiitaTitle()
     }
     
+    // MARK: - Logic
+    
     func getQiitaTitle() {
-        let baseUrl: URL = URL(string: "https://qiita.com/api/v2/items?page=1&per_page=10&query=")!
-        let tags: URL = URL(string: "tag%3AiOS+OR+tag%3ASwift+OR+tag%3AXcode")!
-        let stocks: URL = URL(string: "+stocks%3A%3E100")!
-        let url: URL = URL(string: "\(baseUrl)\(tags)\(stocks)")!
+        let baseUrl = URL(string: "https://qiita.com/api/v2/items?page=1&per_page=30&query=")!
+        let tags = URL(string: "tag%3AiOS+OR+tag%3ASwift+OR+tag%3AXcode")!
+        let stocks = URL(string: "+stocks%3A%3E500")!
+        let url = URL(string: "\(baseUrl)\(tags)\(stocks)")!
         
         let task: URLSessionTask = URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
             do {
@@ -39,21 +45,32 @@ class ViewController: UIViewController, UITableViewDataSource {
                 let count = articles.count
                 for i in 0...count-1 {
                     let title = articles[i]["title"] as! String
+                    let articleUrl = articles[i]["url"] as! String
                     self.titleArray.append(title)
+                    self.urlArray.append(articleUrl)
                 }
                 
                 DispatchQueue.main.sync {
                     self.tableView.reloadData()
                 }
             } catch {
-                print(error)
+                self.alert(title: "失敗", message: "エラー: \(error)")
             }
         })
-        
         task.resume()
     }
     
-    // MARK: - tableView
+    func alert(title:String, message:String) {
+        alertController = UIAlertController(title: title,
+                                            message: message,
+                                            preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK",
+                                                style: .default,
+                                                handler: nil))
+        present(alertController, animated: true)
+    }
+    
+    // MARK: - TableViewDataSource
     
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
@@ -65,6 +82,13 @@ class ViewController: UIViewController, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
         cell.textLabel?.text = titleArray[indexPath.row]
         return cell
+    }
+    
+    // MARK: - TableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let url = URL(string: "\(urlArray[indexPath.row])") else { return  }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
 }
 
