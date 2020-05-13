5ca39bace3593f70452ecd106d7870448f3c2d29
ハナウタ三浦 <miura.hnut@gmail.com>
2020-05-13 18:25:04 +0900

    強制アンラップを修正

--- QiitaApiSample/ViewController.swift
+++ QiitaApiSample/ViewController.swift
@@ -30,10 +30,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // MARK: - Logic
     
     func getQiitaTitle() {
-        let baseUrl = URL(string: "https://qiita.com/api/v2/items?page=1&per_page=30&query=")!
-        let tags = URL(string: "tag%3AiOS+OR+tag%3ASwift+OR+tag%3AXcode")!
-        let stocks = URL(string: "+stocks%3A%3E500")!
-        let url = URL(string: "\(baseUrl)\(tags)\(stocks)")!
+        guard let baseUrl = URL(string: "https://qiita.com/api/v2/items?page=1&per_page=10&query="),
+              let tags = URL(string: "tag%3AiOS+OR+tag%3ASwift+OR+tag%3AXcode"),
+              let stocks = URL(string: "+stocks%3A%3E500"),
+              let url = URL(string: "\(baseUrl)\(tags)\(stocks)")
+              else { return }
         
         let task: URLSessionTask = URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
             do {
@@ -79,15 +80,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else {
+            return UITableViewCell()
+        }
         cell.textLabel?.text = titleArray[indexPath.row]
         return cell
     }
     
     // MARK: - TableViewDelegate
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let url = URL(string: "\(urlArray[indexPath.row])") else { return  }
+    func tableView(_ tableView: UITableView,
+                   didSelectRowAt indexPath: IndexPath) {
+        guard let url = URL(string: "\(urlArray[indexPath.row])") else { return }
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
 }
