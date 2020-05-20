e16822514e27d2ed96574406ceac8b57040728e8
ハナウタ三浦 <miura.hnut@gmail.com>
2020-05-20 22:05:37 +0900

    改行を修正

--- QiitaApiSample/String+.swift
+++ QiitaApiSample/String+.swift
@@ -13,8 +13,10 @@ extension String {
     var urlEncoded: String {
         // 半角英数字 + "/?-._~" のキャラクタセットを定義
         let charset = CharacterSet.alphanumerics.union(.init(charactersIn: "/?-._~"))
+        
         // 一度すべてのパーセントエンコードを除去(URLデコード)
         let removed = removingPercentEncoding ?? self
+        
         // あらためてパーセントエンコードして返す
         return removed.addingPercentEncoding(withAllowedCharacters: charset) ?? removed
     }
